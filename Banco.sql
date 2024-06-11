e7e28bd729ac8f4ebcfb3351abcb8278c4b4789c
davigibelato <davi_gibelato@hoitmail.com>
2024-06-11 20:29:45 -0300

    Banco Atualizado

--- Banco.sql
+++ Banco.sql
@@ -6,10 +6,10 @@ use eliteTech_ds;
 create table usuario (
     idUsuario int primary key auto_increment,
     nome varchar(100) not null,
-    email varchar(100) not null,
+    email varchar(100) not null unique,
     senha varchar(100) not null,
-    cpf char(11) not null,
-    telefone varchar(13),
+    cpf char(11) not null unique,
+    telefone varchar(13) unique,
     status varchar(50) default 'cliente',
     data_registro timestamp default current_timestamp
 );
